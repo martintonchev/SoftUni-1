4d22c183bb7c1caf1ab0ee353e3a719554e49289
Traqn <traqnzlatanov77@gmail.com>
2014-09-24 15:31:55 +0300

    Adding Exception handling Homework

---
+++ Homeworks/OOP/Homework-ExceptionHandling-OOP/EnterNumbers/EnterNumbers.cs
@@ -0,0 +1,56 @@
+﻿namespace EnterNumbers
+{
+    using System;
+
+    class EnterNumbers
+    {
+        static void Main()
+        {
+            Console.WriteLine("Enter lower limit!");
+            int startNumber = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter upper limit!");
+            int endNumber = int.Parse(Console.ReadLine());
+            int num = 0;
+            int[] numbers = new int[10];
+            int counter = 0;
+
+            while (counter < 10)
+            {
+                try
+                {
+                    num = ReadNumber(startNumber, endNumber);
+                    startNumber = num;
+                    numbers[counter] = num;
+                    counter++;
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Invalid Number!");
+                }
+                catch (OverflowException)
+                {
+                    Console.WriteLine("Invalid number! The number is too big to fit in int32!");
+                }
+                catch (ArgumentException)
+                {
+                    Console.WriteLine("The number is not in the requested range!");
+                }
+            }
+        }
+
+        public static int ReadNumber(int startNumber, int endNumber)
+        {
+            int number = 0;
+
+            Console.WriteLine("Enter number between [{0}..{1}]", startNumber, endNumber);
+            number = int.Parse(Console.ReadLine());
+
+            if (number < startNumber || number > endNumber)
+            {
+                throw new ArgumentOutOfRangeException("Number should be in the requested range!");
+            }
+
+            return number;
+        }
+    }
+}
---
+++ Homeworks/OOP/Homework-ExceptionHandling-OOP/SquareRoot/SquareRoot.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class SquareRoot
+{
+    static void Main()
+    {
+        try
+        {
+            string s = Console.ReadLine();
+            double num = double.Parse(s);
+            if (num < 0)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+            Console.WriteLine("Sqrt = " + Math.Sqrt(num));
+        }
+        catch (FormatException)
+        {
+            Console.WriteLine("Invalid number");
+        }
+        catch (ArgumentOutOfRangeException)
+        {
+            Console.WriteLine("Number should be positive!");
+        }
+        finally
+        {
+            Console.WriteLine("Good bye");
+        }
+    }
+}
+
